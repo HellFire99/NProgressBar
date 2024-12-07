6cf59654a82275e1d867c1d365c40542ac9f0aef
GitHub Action <action@github.com>
2024-12-07 13:07:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/hellfire99/nprogressbar/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/hellfire99/nprogressbar/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.hellfire99.nprogressbar.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hellfire99.nprogressbar.MyBundle
+import com.github.hellfire99.nprogressbar.services.MyProjectService
 import javax.swing.JButton
 
 
